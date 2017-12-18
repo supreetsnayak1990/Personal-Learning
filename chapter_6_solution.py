91be08c6ac99d68cf524607b0247556e9ff6eeb8
Supreet Nayak <supreetsnayak1990@users.noreply.github.com>
2017-12-18 14:50:41 -0600

    Add files via upload

--- chapter_6_solution.py
+++ chapter_6_solution.py
@@ -32,9 +32,43 @@ def pali(word):
 		return True
 	else:
 		return False
+		
+def pow(a,b):
+	if((b%a == 0) and (pow((a/b),b))):
+		return True
+	else:
+		return False
 
+def gcd(a,b):
+	if(b==0):
+		return a
+	elif(b==1):
+		return 1
+	else:
+		return gcd(b,a%b)
+		
+import math
+def estimate_pi():
+    total = 0
+    k = 0
+    equation = 0
+    factor = (2*math.sqrt(2))/9801
+    while True:
+        num = math.factorial(4*k) * (1103+(26390*k))
+        den = (math.factorial(k)**4) * (396**(4*k))
+        term = factor * num / den
+        total += term
+        k = k+1
+        if (abs(term) < (1e-15)):
+            break
+    return 1/ total
+		
 		
 print fact(5)
 print fibonacci(4)
 print pali("redivider")
 print pali("Supreet")
+print pow(16.0,2.0)
+print gcd(15,36)
+print gcd(20,13)
+print estimate_pi()
